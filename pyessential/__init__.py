db66dfa7056ad37da72a4e97d8967bd99061a1c6
Abhi39054 <abhishekkumar39054@gmail.com>
2025-11-19 21:54:04 +0530

    Pypi publish - test4

--- pyessential/__init__.py
+++ pyessential/__init__.py
@@ -4,7 +4,7 @@ from .generators import generate_random_int, generate_secret_key
 from .logger import Logger
 
 # Version and Author Info
-__version__ = "0.1.0" 
+__version__ = "0.1.1" 
 __author__ = "Abhishek Kumar"
 
 
